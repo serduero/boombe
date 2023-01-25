8752c49e0ef7c35a12d8598c81986e1adfc68f55
unknown <serduero@gmail.com>
2023-01-25 23:50:13 +0100

    nuevos eventos

--- src/controllers/websocket.js
+++ src/controllers/websocket.js
@@ -5,18 +5,28 @@ import { newPlayer, removePlayer, getTurno } from "./database.js";
 //
 export const tratamiento_Socket = (io) => {
   io.on("connection", (socket) => {
-    // guadamos el jugador. Retorna el número total de jugadores tras inserción
-    var numUsers = newPlayer(socket.id);
-
-    // enviamos a todos (incluido yo) el número de conectados
-    io.sockets.emit("bomb:users", numUsers);
-
-    // console.log(`nueva conexión ${socket.id} ;usuarios: ${numUsers}`);
+    // console.log(`nueva conexión ${socket.id}`);
     /**
      * Eventos
      */
 
-    // bomb:accion ==>
+    // primer evento siempre tras conectarse:
+    // recibimos el nickname del nuevo jugador conectado
+    socket.on("cli:nick", (nickname) => {
+      var numUsers = newPlayer(socket.id, nickname);
+      // console.log(
+      //   `recibido de ${socket.id} el nick ${nickname} users: ${numUsers}`
+      // );
+
+      // si partida ya iniciada no dejamos a más jugadores
+      if (numUsers === -1) {
+        socket.disconnect(true);
+      }
+
+      // enviamos a todos (incluido yo) el número de conectados
+      io.sockets.emit("ser:nump", numUsers);
+    });
+
     socket.on("bomb:accion", ({ senderId, text, time }) => {
       // console.log(`accion de cliente ${senderId} ${time}`);
       // console.log(text);
@@ -25,13 +35,14 @@ export const tratamiento_Socket = (io) => {
       // console.log(`turno: ${turno}`);
     });
 
-    // disconnect ==> se ha desconectado el jugador
+    // se ha desconectado el jugador, lo quitemos de la lista y
+    // actualizamos número de los conectados a todos
     socket.on("disconnect", () => {
       // console.log(`desconectado ${socket.id}`);
-      numUsers = removePlayer(socket.id);
+      var numUsers = removePlayer(socket.id);
 
       // actualizamos enviando a todos el número de conectados
-      io.sockets.emit("bomb:users", numUsers);
+      io.sockets.emit("ser:fin", numUsers);
     });
   });
 };
